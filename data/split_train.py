385f49002c7ba902e16eddfa4a391a2272aec6f2
Alain Tanguy <alain.tanguy@mfglabs.com>
2019-07-24 10:45:27 +0200

    split_train.dvc

---
+++ data/split_train.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+cwd = os.getcwd()
+
+train = pd.read_csv("{}/data/train.csv".format(cwd))
+train = train.sample(frac=1)
+shape = int(train.shape[0] * 0.8)
+
+learning_train = train.iloc[:shape, :]
+learning_test = train.iloc[shape:, :]
+
+learning_train.to_csv("{}/data/learning_train.csv".format(cwd),index=False)
+learning_test.to_csv("{}/data/learning_test.csv".format(cwd),index=False)
